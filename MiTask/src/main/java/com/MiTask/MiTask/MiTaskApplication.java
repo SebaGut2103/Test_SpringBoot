d080a7f59871a9395a9f107a9cd70144c8376c0b
¨SebGut2103¨ <merlina@yahoo.com.ar.pe.ch.ve.br>
2025-06-25 19:09:16 -0500

    ¨subiendoProyecto¨

---
+++ MiTask/src/main/java/com/MiTask/MiTask/MiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.MiTask.MiTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiTaskApplication.class, args);
+	}
+
+}
---
+++ MiTask/src/test/java/com/MiTask/MiTask/MiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MiTask.MiTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
